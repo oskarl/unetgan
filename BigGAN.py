274aa4f6807e1ca72a78d4df439f08a478ed3c11
oskarla <oskar.larsson@onewave.se>
2021-03-14 19:06:49 +0100

    fix

--- BigGAN.py
+++ BigGAN.py
@@ -209,7 +209,7 @@ class Generator(nn.Module):
                                                      betas=(self.B1, self.B2), weight_decay=0,
                                                      eps=self.adam_eps)
         else:
-            self.optim = ExtraAdam(params=self.parameters(), lr=self.lr,
+            self.optim = extragradient.ExtraAdam(params=self.parameters(), lr=self.lr,
                                                      betas=(self.B1, self.B2), weight_decay=0,
                                                      eps=self.adam_eps)
 
@@ -467,7 +467,7 @@ class Unet_Discriminator(nn.Module):
             self.optim = utils.Adam16(params=self.parameters(), lr=self.lr,
                                                          betas=(self.B1, self.B2), weight_decay=0, eps=self.adam_eps)
         else:
-            self.optim = ExtraAdam(params=self.parameters(), lr=self.lr,
+            self.optim = extragradient.ExtraAdam(params=self.parameters(), lr=self.lr,
                                                          betas=(self.B1, self.B2), weight_decay=0, eps=self.adam_eps)
         # LR scheduling, left here for forward compatibility
         # self.lr_sched = {'itr' : 0}# if self.progressive else {}
