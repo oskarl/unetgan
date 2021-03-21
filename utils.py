ea5a33e5da1c72a8323138e41db3e332755c86d4
oskarla <oskar.larsson@onewave.se>
2021-03-21 21:10:52 +0100

    m

--- utils.py
+++ utils.py
@@ -1026,7 +1026,7 @@ def interp_sheet(G, num_per_sheet, num_midpoints, num_classes, parallel,
     else:
      out_ims = G(zs, ys).data.cpu()
   interp_style = '' + ('Z' if not fix_z else '') + ('Y' if not fix_y else '')
-  image_filename = '../interp%d.jpg' % (samples_root, sheet_number)
+  image_filename = '../interp%d.jpg' % (sheet_number)
   print(image_filename)
   torchvision.utils.save_image(out_ims, image_filename,
                                nrow=num_midpoints + 2, normalize=True)
