691e1e131b6526130a400827edcad44a25bc5d52
oskarla <oskar.larsson@onewave.se>
2021-03-21 21:29:49 +0100

    m

--- utils.py
+++ utils.py
@@ -1004,7 +1004,7 @@ def interp_sheet(G, num_per_sheet, num_midpoints, num_classes, parallel,
     z0 = torch.randn(num_per_sheet, 1, G.dim_z, device=device)/2
     z1 = torch.randn(num_per_sheet, 1, G.dim_z, device=device)/2
     z2 = torch.randn(num_per_sheet, 1, G.dim_z, device=device)/2
-    z3 = torch.randn(num_per_sheet, 1, G.dim_z, device=device)2
+    z3 = torch.randn(num_per_sheet, 1, G.dim_z, device=device)/2
     zs = interp(z0,
                 z1,
                 num_midpoints).view(-1, G.dim_z)
